5e2a762c4496d461b49f3bdc3ddba4dd1ca1b9a7
felipe <tifonzafel@gmail.com>
2022-01-17 22:26:17 +0100

    introducing pipe in websocket

--- src/interfaces/websocket.rs
+++ src/interfaces/websocket.rs
@@ -10,12 +10,14 @@ use async_tungstenite::async_std::{connect_async, ConnectStream};
 use async_tungstenite::tungstenite::protocol::Message;
 use async_tungstenite::WebSocketStream;
 
+use crate::pipe::Pipe;
 use crate::utils;
 use async_std::task;
 use futures::stream::{SplitSink, SplitStream};
 use futures::{try_join, SinkExt, StreamExt};
 use serde_json::Value;
 use std::time::Duration;
+
 #[derive(Debug)]
 pub struct WebsocketConsumer {
     pub url: String,
@@ -31,16 +33,17 @@ impl WebsocketConsumer {
             subscription_message,
         }
     }
-    pub async fn consume(
+    pub async fn consume<P: 'static + Pipe>(
         &self,
         channel_sender: async_channel::Sender<Value>,
+        ops: P,
     ) -> utils::GenericResult<()> {
         let (websocket, _) = connect_async(&self.url).await.unwrap();
         let (mut websocket_sender, websocket_receiver) = websocket.split();
 
         Self::subscribe(&mut websocket_sender, self.subscription_message.clone()).await?;
         try_join!(
-            task::spawn(Self::listen(websocket_receiver, channel_sender)),
+            task::spawn(Self::listen(websocket_receiver, channel_sender, ops)),
             self.send_heartbeat(websocket_sender)
         )?;
         Ok(())
@@ -54,23 +57,25 @@ impl WebsocketConsumer {
         Ok(())
     }
 
-    async fn listen(
+    async fn listen<P: 'static + Pipe>(
         mut receiver: SplitStream<WebSocketStream<ConnectStream>>,
         mut channel_sender: async_channel::Sender<Value>,
+        ops: P,
     ) -> utils::GenericResult<()> {
         while let Some(msg) = receiver.next().await {
             let event = msg.unwrap().into_text().unwrap();
-            Self::send_data_to_channel(&event, &mut channel_sender).await?;
+            Self::send_data_to_channel(&event, &mut channel_sender, &ops).await?;
         }
         Ok(())
     }
 
-    async fn send_data_to_channel(
+    async fn send_data_to_channel<P: 'static + Pipe>(
         event: &str,
         channel_sender: &mut async_channel::Sender<Value>,
+        ops: &P,
     ) -> utils::GenericResult<()> {
         let dict: Value = serde_json::from_str(event)?;
-        if dict.get("key").is_some() {
+        if ops.filter(&dict) {
             channel_sender.send(dict).await?
         };
         Ok(())
--- src/pipe.rs
+++ src/pipe.rs
@@ -1,10 +1,6 @@
 use serde_json::Value;
 
-pub trait Pipe {
-    fn filter<F>(fun: F, val: &Value) -> bool
-    where
-        F: Fn(&Value) -> bool;
-    fn map<F>(fun: F, val: &Value) -> &Value
-    where
-        F: Fn(&Value) -> &Value;
+pub trait Pipe: Send + Sync {
+    fn filter(&self, val: &Value) -> bool;
+    fn map(&self, val: &Value) -> &Value;
 }
